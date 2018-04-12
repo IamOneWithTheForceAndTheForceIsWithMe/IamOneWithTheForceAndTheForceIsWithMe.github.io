c1bad744570b9cb4b6ae19f35c3c61745fbe611b
LuckIamYourFather <10a014k@gmail.com>
2018-04-12 12:18:26 +0300

    Naming

--- javascript/index.js
+++ javascript/index.js
@@ -2,51 +2,51 @@
 
 (function() {
 
-    const buttonsArray = document.getElementsByClassName("buttonsClass");
-    const cleaningButton = document.getElementById("clearingButtonId");
-    const backSpaceButton = document.getElementById("backSpaceButtonId");
-    const equalingButton = document.getElementById("equalingButtonId");
-    const arrowButton = document.getElementById("arrowButtonId");
-    const manual = document.getElementById("manualId");
-    const arrowImage = document.getElementById("arrowImageId");
-    let inputString = document.getElementById("inputId");
-    let result = document.getElementById("resultId");
+    const $buttonsArray = document.getElementsByClassName("buttonsClass");
+    const $cleaningButton = document.getElementById("clearingButtonId");
+    const $backSpaceButton = document.getElementById("backSpaceButtonId");
+    const $equalingButton = document.getElementById("equalingButtonId");
+    const $arrowButton = document.getElementById("arrowButtonId");
+    const $manual = document.getElementById("manualId");
+    const $arrowImage = document.getElementById("arrowImageId");
+    let $inputString = document.getElementById("inputId");
+    let $result = document.getElementById("resultId");
     actionAssigning();
 
     function actionAssigning() {
-        for (let i = 0; i < buttonsArray.length; i++){
-            buttonsArray[i].addEventListener('click', element =>{
+        for (let i = 0; i < $buttonsArray.length; i++){
+            $buttonsArray[i].addEventListener('click', element =>{
                 element.preventDefault();
-                inputString.innerHTML +=buttonsArray[i].value;
+                $inputString.innerHTML +=$buttonsArray[i].value;
             })
         }
-        cleaningButton.onclick = element =>{
+        $cleaningButton.onclick = element =>{
             element.preventDefault();
-            inputString.innerHTML = "";
-            result.innerHTML = "0";
+            $inputString.innerHTML = "";
+            $result.innerHTML = "0";
         };
-        backSpaceButton.onclick = element =>{
+        $backSpaceButton.onclick = element =>{
             element.preventDefault();
-            inputString.innerHTML = inputString.innerHTML.slice(0,-1);
+            $inputString.innerHTML = $inputString.innerHTML.slice(0,-1);
         };
-        equalingButton.onclick = element =>{
+        $equalingButton.onclick = element =>{
             element.preventDefault();
-            let expression = eval(inputString.innerHTML);
+            let expression = eval($inputString.innerHTML);
             if (isNaN(expression)){
-                result.innerHTML = "Entered expression is not correct!"
+                $result.innerHTML = "Entered expression is not correct!"
             } else {
-                result.innerHTML = expression;
+                $result.innerHTML = expression;
             }
         };
-        arrowButton.addEventListener("click", () =>{
-            let visibilityValue = manual.style.visibility;
+        $arrowButton.addEventListener("click", () =>{
+            let visibilityValue = $manual.style.visibility;
             if (visibilityValue === "visible"){
-                manual.style.visibility = "hidden";
-                arrowImage.src = "css/pictures/right_arrow.png";
+                $manual.style.visibility = "hidden";
+                $arrowImage.src = "css/pictures/right_arrow.png";
 
             } else {
-                manual.style.visibility = "visible";
-                arrowImage.src = "css/pictures/left_arrow .png";
+                $manual.style.visibility = "visible";
+                $arrowImage.src = "css/pictures/left_arrow .png";
             }
         });
     }
